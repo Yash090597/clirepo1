ac8727c3fc00934895bf70b0454dbb60292384f9
Yash090597 <yashlodalliwar@gmail.com>
2021-07-07 17:11:11 +0530

    main method updated

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,8 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+	System.out.println("qwertt");
+	System.out.println("hi");
+	System.out.println("ABCD"); 
     }
 }
